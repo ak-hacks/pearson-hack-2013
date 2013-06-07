cd0458454d1d83799f3616f5c1615d2e0550124e
Caroline Handley <caroline.handley@ft.com>
2013-06-07 11:21:50 +0100

    Recalibrated pretty much best working version\!

--- src/main/webapp/js/UserInterface.js
+++ src/main/webapp/js/UserInterface.js
@@ -1,12 +1,13 @@
-/*global $, alert, console*/
+/*global $, console*/
 //react to the events
-var UserInterface = function (yOffsetVal, xfactorVal) {
-    var yOffset = yOffsetVal,
-        xfactor = xfactorVal,
+var UserInterface = function (xfactorVal, yfactorVal) {
+    var xfactor = xfactorVal,
+        yfactor = yfactorVal,
         xposition = 0,
         yposition = 0,
         halfWindowWidth = Math.floor($(window).width() / 2 ),
-        currentElem;
+        elemOpen = false,
+        openEventInProgress = false;
 
     $('body').append('<img src="/leap-pearson/img/blob.png" alt="location indicator" id="marker-blob">');
 
@@ -30,40 +31,57 @@ var UserInterface = function (yOffsetVal, xfactorVal) {
     }
 
     function open(xpos, ypos) {
-        var xlocation = halfWindowWidth + parseInt(xpos, 10),
-            ylocation = $(window).height() - parseInt(ypos, 10); //we're sent y from the bottom up, method expects y from top left corner
-        if (xlocation > 0 && ylocation > 0) {
-            currentElem = document.elementFromPoint(xlocation, ylocation);
-            // console.log('xlocation: ', xlocation, ' ylocation: ', ylocation, ' elem is', elem);
-            //alert('success \n received: [' + xpos+','+ ypos+ '] translated to: ['+xlocation+','+ylocation+']');
-            currentElem .click();
+        if (!openEventInProgress) {
+            openEventInProgress = true;
+            var xlocation = halfWindowWidth + (parseInt(xpos, 10) * xfactor),
+                ylocation = $(window).height() - (parseInt(ypos, 10) * yfactor); //we're sent y from the bottom up, method expects y from top left corner
+            if (xlocation > 0 && ylocation > 0) {
+                var elem = document.elementFromPoint(xlocation, ylocation);
+                console.log('xlocation: ', xlocation, ' ylocation: ', ylocation, ' elem is', elem);
+                //alert('success \n received: [' + xpos+','+ ypos+ '] translated to: ['+xlocation+','+ylocation+']');
+                if ($(elem).attr('id') !== "marker-blob") {
+                    elem.click();
+                    elemOpen = true;
+                }
+            } else {
+                console.log('fail \n received: [' + xpos+','+ ypos+ '] translated to: ['+xlocation+','+ylocation+']');
+            }
+            setTimeout(function () {
+                openEventInProgress = false;
+            }, 2000);
         } else {
-            console.log('fail \n received: [' + xpos+','+ ypos+ '] translated to: ['+xlocation+','+ylocation+']');
+            console.log('open received whilst another open already in progress');
         }
+
     }
 
     function close() {
-        $('div#overlay').click();
+        if (elemOpen) {
+            elemOpen = false;
+            $('div#overlay').click();
+        } else {
+            console.log('close received with no open prior');
+        }
     }
 
     function hover(xpos, ypos) {
-        xposition = xpos;
-        yposition = ypos;
+        // console.log('received: xpos:', xpos, ' ypos:', ypos);
+        xposition = xpos * xfactor;
+        yposition = ypos * yfactor;
         updateBlob();
     }
 
     function updateBlob() {
-        var multipliedx = xposition * xfactor,
-            newX = halfWindowWidth + (xposition * xfactor),
-            newY = yposition + yOffset;
+        var newX = halfWindowWidth + xposition,
+            newY = yposition;
 
-        console.log('multipliedx', multipliedx, 'x is', newX, 'y is', newY);
+        // console.log(' xpos:', xposition, ' ypos:', yposition, ' || x is', newX, 'y is', newY);
         $('#marker-blob').css({left: newX, bottom: newY});
     }
 
     return {
         xfactor: xfactor,
-        yOffset: yOffset,
+        yfactor: yfactor,
         hover: hover,
         open: open,
         close: close,
--- src/main/webapp/js/sockets.js
+++ src/main/webapp/js/sockets.js
@@ -7,25 +7,25 @@ Pusher.log = function(message) {
 var pusher = new Pusher('f17a457dad3779afdba1');
 var channel = pusher.subscribe('leap_pearson');
 
-var ui = new UserInterface(200, 3);
+var ui = new UserInterface(3, 2.6);
 
 channel.bind('swipe', function(data) {
-    console.log('swipe received', data);
+    // console.log('swipe received', data);
     ui.swipe(data.segment, data.direction);
 });
 
 channel.bind('open', function(data) {
-    console.log('open received', data);
+    // console.log('open received', data);
     ui.open(data.x, data.y);
 });
 
 channel.bind('close', function() {
-    console.log('close received');
+    // console.log('close received');
     ui.close();
 });
 
 
 channel.bind('hover', function(data) {
-    console.log('hover received', data);
+    // console.log('hover received', data);
     ui.hover(data[0], data[1]);
 });
